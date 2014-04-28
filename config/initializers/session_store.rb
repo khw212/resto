9c8326b78e7f64a7c03fae52ff194f67a8fd7e0a
Kong Wang <khw212@gmail.com>
2014-04-28 14:41:41 -0400

    Update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,7 +5,7 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key    => "_RESTO_session",
+  :key    => "_my_session",
   :secret => "5b04221191dac7dfddv5bf4a2270704e"
 }
 
